4aa16b5c17e950690bc8eaf6922dc349d728b18e
dqwdd <dqwdd@naver.com>
2021-09-06 17:44:02 +0900

    오늘의 마지막 커밋 뭘 추가했는진 모름

--- app/src/main/java/com/neppplus/colosseum_20210903/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20210903/ViewTopicDetailActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.neppplus.colosseum_20210903.datas.TopicData
+import com.neppplus.colosseum_20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_view_topic_detail.*
 
 class ViewTopicDetailActivity : BaseActivity() {
@@ -27,5 +28,15 @@ class ViewTopicDetailActivity : BaseActivity() {
         Glide.with(mContext).load(mTopicData.imageURL).into(topicImg)
         titleTxt.text = mTopicData.title
 
+//        나머지 데이터는 서버에서 가져오자
+        getTopicDetailDataFromServer()
     }
+
+//    투표 현황 등, 최신 토론 상세 데이터를 다시 서버에서 불러오기
+
+    fun getTopicDetailDataFromServer() {
+        ServerUtil
+    }
+
+
 }
