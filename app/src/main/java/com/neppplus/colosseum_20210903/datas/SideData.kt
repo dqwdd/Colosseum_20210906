75012c5f6a2acfce9bc97245e5d158bcb5860ab6
dqwdd <dqwdd@naver.com>
2021-09-07 10:07:47 +0900

    토론 주제를 파싱할 때 -> 선택 진영 목록도 같이 파싱 - 토론 주제의 sideList에 목록으로 - SideData에 json -> SideData()변환 기능 추가 / 활용

--- app/src/main/java/com/neppplus/colosseum_20210903/datas/SideData.kt
+++ app/src/main/java/com/neppplus/colosseum_20210903/datas/SideData.kt
@@ -1,5 +1,7 @@
 package com.neppplus.colosseum_20210903.datas
 
+import org.json.JSONObject
+
 class SideData(
     var id: Int,
     var topicId: Int,
@@ -7,5 +9,23 @@ class SideData(
     var voteCount: Int) {
 
     constructor() : this(0, 0, "", 0)//기본값들
+
+    companion object {
+
+//        JSON을 넣으면 -> SideData로 변환해주는 기능
+        fun getSideDataFromJson(json : JSONObject) : SideData{
+
+            val sideData = SideData()
+
+            sideData.id = json.getInt("id")
+            sideData.topicId = json.getInt("topic_id")
+            sideData.title = json.getString("title")
+            sideData.voteCount = json.getInt("vote_count")
+
+            return sideData
+
+        }
+
+    }
 
 }
