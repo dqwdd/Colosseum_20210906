b7ddbd46f81f016d32dee3ccf938f82119e3fb74
dqwdd <dqwdd@naver.com>
2021-09-08 12:09:16 +0900

    알림 목록 보여주는 화면 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20210903/NotificationListActivity.kt
@@ -0,0 +1,19 @@
+package com.neppplus.colosseum_20210903
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationListActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_list)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
