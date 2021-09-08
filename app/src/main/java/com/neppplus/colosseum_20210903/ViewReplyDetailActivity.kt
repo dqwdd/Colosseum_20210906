af46756084ba13f6db130d069dc1898559e1c06b
dqwdd <dqwdd@naver.com>
2021-09-08 16:55:28 +0900

    답글 달기 기능까지 완료 -키보드 자동 숨김 처리 예정

--- app/src/main/java/com/neppplus/colosseum_20210903/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20210903/ViewReplyDetailActivity.kt
@@ -2,9 +2,12 @@ package com.neppplus.colosseum_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.neppplus.colosseum_20210903.datas.ReplyData
 import com.neppplus.colosseum_20210903.datas.SideData
+import com.neppplus.colosseum_20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_view_reply_detail.*
+import org.json.JSONObject
 
 class ViewReplyDetailActivity : BaseActivity() {
 
@@ -18,6 +21,29 @@ class ViewReplyDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        okBtn.setOnClickListener {
+            val inputContent = contentEdt.text.toString()
+
+            if (inputContent.length<5){
+                Toast.makeText(mContext, "5글자 이상 입력하세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            ServerUtil.postRequestChileReply(mContext, inputContent, mReplyData.id, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    runOnUiThread {
+                        contentEdt.setText("")
+                    }
+
+
+                }
+
+            })
+
+        }
+
     }
 
     override fun setValues() {
