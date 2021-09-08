c15b5acfd30725411503e544c3ee38693938f13d
dqwdd <dqwdd@naver.com>
2021-09-08 17:00:16 +0900

    도전코드 작성(구글링) - 키보드 숨김처리

--- app/src/main/java/com/neppplus/colosseum_20210903/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20210903/ViewReplyDetailActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.colosseum_20210903
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.neppplus.colosseum_20210903.datas.ReplyData
 import com.neppplus.colosseum_20210903.datas.SideData
@@ -35,6 +37,11 @@ class ViewReplyDetailActivity : BaseActivity() {
 
                     runOnUiThread {
                         contentEdt.setText("")
+
+//                        도전코드(구글링) : 키보드 숨김 처리
+                        val imm = mContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+
                     }
 
 
