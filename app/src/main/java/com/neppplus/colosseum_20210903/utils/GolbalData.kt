233f6d3a4dc27c8712d13c6c2411cb16e9c23adc
dqwdd <dqwdd@naver.com>
2021-09-09 11:55:00 +0900

    모든 프로젝트에 공유할 로그인 한 사용자 정보를 저장할 클랠스 별도로 생성 - 로그인한 사용자 변수 추가 : 기본값 - 그런 사람 없다(null)

---
+++ app/src/main/java/com/neppplus/colosseum_20210903/utils/GolbalData.kt
@@ -0,0 +1,14 @@
+package com.neppplus.colosseum_20210903.utils
+
+import com.neppplus.colosseum_20210903.datas.UserData
+
+class GolbalData {
+
+    companion object {
+
+//        로그인한 사용자를 담아둘 변수
+
+        val loginUser : UserData? = null//앱이 처음 켜졌을 때는 로그인 한 사용자가 없다는걸 표시한 코드
+
+    }
+}
